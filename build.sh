ca850ccb7177d500aa8c6a4bc314007ab052d7b0
opop4m <opop4m@protonmail.com>
2021-12-22 22:43:14 +0800

    f

--- build.sh
+++ build.sh
@@ -7,15 +7,28 @@ if [ "${DOMAIN}" == "**None**" ]; then
 fi
 
 cd ${MY_FILES}
-if [ ! -f "${MY_FILES}/base.pem" ]; then
-    openssl genrsa -out base.key 2048
-    openssl req -new -x509 -nodes -key base.key -days 10000 -subj "/CN=${DOMAIN}" -out base.pem
-    openssl genrsa -out device.key 2048
-    openssl req -new -key device.key -subj "/CN=${DOMAIN}" -addext "subjectAltName = DNS:${DOMAIN}" -out device.csr
-    openssl x509 -req -in device.csr -CA base.pem -CAkey base.key -CAcreateserial -days 10000 -out device.crt
+if [ ! -f "${MY_FILES}/ca.crt" ]; then
+    openssl genrsa -out ca.key 2048
+    openssl req -new -x509 -nodes -key ca.key -days 10000 -subj "/CN=${DOMAIN}" -out ca.crt
+
+    openssl req -new -sha256 \
+    -key ca.key \
+    -subj "/C=CN/ST=Beijing/L=Beijing/O=UnitedStack/OU=Devops/CN=${DOMAIN}" \
+    -reqexts SAN \
+    -config <(cat /etc/pki/tls/openssl.cnf \
+        <(printf "[SAN]\nsubjectAltName=DNS:${DOMAIN}")) \
+    -out device.csr
+
+    openssl x509 -req -days 365000 \
+    -in device.csr -CA ca.crt -CAkey ca.key -CAcreateserial \
+    -extfile <(printf "subjectAltName=DNS:${DOMAIN}") \
+    -out server.crt
+    # openssl genrsa -out device.key 2048
+    # openssl req -new -key device.key -subj "/CN=${DOMAIN}" -addext "subjectAltName = DNS:${DOMAIN}" -out device.csr
+    # openssl x509 -req -in device.csr -CA base.pem -CAkey base.key -CAcreateserial -days 10000 -out device.crt
 fi
 
-cp -r base.pem /ngrok/assets/client/tls/ngrokroot.crt
+cp -r ca.crt /ngrok/assets/client/tls/ngrokroot.crt
 
 cd /ngrok
 
@@ -23,13 +36,13 @@ go env GO111MODULE=off
 git config --global http.sslverify false
 
 make release-server
-GOOS=linux GOARCH=386 make release-client
-GOOS=linux GOARCH=amd64 make release-client
-GOOS=windows GOARCH=386 make release-client
-GOOS=windows GOARCH=amd64 make release-client
-GOOS=darwin GOARCH=386 make release-client
-GOOS=darwin GOARCH=amd64 make release-client
-GOOS=linux GOARCH=arm make release-client
+# GOOS=linux GOARCH=386 make release-client
+# GOOS=linux GOARCH=amd64 make release-client
+# GOOS=windows GOARCH=386 make release-client
+# GOOS=windows GOARCH=amd64 make release-client
+# GOOS=darwin GOARCH=386 make release-client
+# GOOS=darwin GOARCH=amd64 make release-client
+# GOOS=linux GOARCH=arm make release-client
 
 cp -r /ngrok/bin ${MY_FILES}/bin
 
